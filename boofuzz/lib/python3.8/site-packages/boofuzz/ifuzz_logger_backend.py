9710d27a3258a551a072dd7467dcf01e92779c58
jackfromeast <jackfromeast@161.113.210.222.broad.cd.sc.dynamic.163data.com.cn>
2021-03-21 22:47:34 +0800

    delete boofuzz package which is too big to push

--- boofuzz/lib/python3.8/site-packages/boofuzz/ifuzz_logger_backend.py
+++
@@ -1,7 +0,0 @@
-from . import ifuzz_logger
-
-# IFuzzLoggerBackend is identical to IFuzzLogger at this point, so just copy:
-IFuzzLoggerBackend = ifuzz_logger.IFuzzLogger
-"""
-Interface for backends for FuzzLogger. Currently identical to IFuzzLogger.
-"""
